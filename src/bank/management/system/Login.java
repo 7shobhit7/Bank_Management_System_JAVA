a9ad41f91964c2357373d3b941a5f368123666b6
Shobhit Agarwal <shobhit7agarwal7@gmail.com>
2023-07-21 11:16:49 +0530

    init

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,452 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.text.*;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.BorderFactory;
+
+
+public class Login extends JFrame implements ActionListener
+{
+    
+    
+    //GLOBAL VARIABLES
+    JButton login,clear,signUp,reset;
+    JTextField cardInput;
+    JPasswordField pinInput;
+    public Login() 
+    {
+        JFrame frame = new JFrame("SUPER ANY TIME MACINE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //This will make this image as background 
+        frame.setContentPane(new ImagePanel(new ImageIcon(getClass().getResource("/icons/itachi2.jpg")).getImage()));
+        frame.setLayout(null);
+        
+        //WELCOME TO NETBEANS
+        JLabel label = new JLabel("WELCOME TO NETBEANS");
+        label.setFont(new Font("Osward",Font.BOLD,40));
+        label.setForeground(new Color(250, 249, 246));//24,30,33
+        int CustomY=20;
+        myCustom(frame,label,CustomY);
+        frame.add(label);
+        
+        
+        //CardNumber
+        JLabel cardno=new JLabel("CARD NUMBER :");
+        cardno.setFont(new Font("Osward",Font.BOLD,30));
+         cardno.setForeground(new Color(250, 249, 246));
+        frame.add(cardno);
+        int Custom2X=145;
+        int Custom2Y=140;
+        myCustom2(frame,cardno,Custom2X,Custom2Y);
+        //CardInput
+        cardInput=new JTextField();
+        int Custom2X_cardInput=150;
+        int Custom2Y_cardInput=250;
+        int Custom2Z_cardInput=10;
+        myCustom2_Input(frame,cardInput,Custom2X_cardInput,Custom2Y_cardInput,Custom2Z_cardInput);
+        addNumericInputConstraint(cardInput);
+        addNumericInputConstraint_message(cardInput);
+        addNumericInputConstraint_BOLD(cardInput);
+       
+        frame.add(cardInput);
+        
+        //PIN number
+        JLabel pinNumber=new JLabel("ENTER YOUR PIN HERE  :");
+        pinNumber.setFont(new Font("Osward",Font.BOLD,30));
+        pinNumber.setForeground(new Color(250, 249, 246));
+        frame.add(pinNumber);
+        myCustom3(frame,pinNumber);
+        //CardInput
+        pinInput=new JPasswordField();
+        myCustom3_input(frame,pinInput);
+        addNumericInputConstraint(pinInput);
+        addNumericInputConstraint_message(pinInput);
+        addNumericInputConstraint_BOLD(pinInput);
+        frame.add(pinInput);
+       
+        
+        //Login Button
+         login=new JButton("LOGIN");
+        myCustom4(frame,login);
+         login.addActionListener(this);
+        frame.add(login);
+        
+        //Clear Button
+        clear=new JButton("CLEAR");
+        myCustom5(frame,clear);
+        clear.addActionListener(this);
+        frame.add(clear);
+        
+        //SignUP Button
+        signUp=new JButton("SIGN UP");
+        myCustom6(frame,signUp);
+         signUp.addActionListener(this);
+        frame.add(signUp);
+        
+        //RESET Button
+        reset=new JButton("RESET");
+        myCustom7(frame,reset);
+        addClearButtonFunctionality(frame,reset);
+        frame.add(reset);
+        
+        
+        int x=800;
+        int y=600;
+        frame.setSize(x, y);
+        enforceMinimumFrameSize(frame,x,y);
+        frame.setLocationRelativeTo(null); // Center the frame
+        frame.setVisible(true);
+    }
+
+    
+    
+    
+    //myCustom made heading
+    public static void myCustom(JFrame frame,JLabel label,int CustomY)
+    {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=label.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 label.setBounds(labelX, CustomY, labelSize.width, labelSize.height);       
+             }
+         });
+    }
+    
+    
+    
+    
+        //myCustom for cardno
+        public static void myCustom2(JFrame frame,JLabel cardno,int Custom2X,int Custom2Y)
+         {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=cardno.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 cardno.setBounds(labelX-Custom2X, Custom2Y, labelSize.width, labelSize.height);    
+             }
+         });
+      }
+         //Mycustom2 for cardInput
+        public static void myCustom2_Input(JFrame frame,JTextField cardInput,int Custom2X_cardInput,int Custom2Y_cardInput,int Custom2Z_cardInput)
+        {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=cardInput.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 cardInput.setBounds(labelX, Custom2X_cardInput, labelSize.width+Custom2Y_cardInput, labelSize.height+Custom2Z_cardInput);//150 250 10
+                 
+             }
+         });
+    }
+    
+        
+        
+        
+        
+        
+        
+     //MyCustom for pinNumber 
+    public static void myCustom3(JFrame frame,JLabel pinNumber)
+         {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=pinNumber.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 pinNumber.setBounds(labelX-210, 200, labelSize.width, labelSize.height);    
+             }
+         });
+      }
+    //Mycustom for pinInput
+    public static void myCustom3_input(JFrame frame,JTextField pinInput)
+     {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=pinInput.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 pinInput.setBounds(labelX, 205, labelSize.width+250, labelSize.height+10);
+             
+                 
+                 
+             }
+         });
+      }
+    
+    
+    
+    
+    //LOGIN BUTTON Custom
+    public static void myCustom4(JFrame frame, JButton login) 
+    {
+    frame.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            int frameWidth = frame.getWidth();
+            int frameHeight = frame.getHeight();
+            int buttonWidth = login.getWidth();
+            int buttonHeight = login.getHeight();
+            int x = (frameWidth - buttonWidth) / 2;
+            int y = (frameHeight - buttonHeight) / 2;
+            int xOffset = 0; // Specify the desired horizontal offset from the center
+            int yOffset = 10; // Specify the desired vertical offset from the center
+            login.setBounds(x + xOffset-150, y + yOffset+20,150,30);
+            login.setBorder(BorderFactory.createLineBorder(new Color(139, 0, 0), 2));
+        }
+    });
+}
+      public static void myCustom5(JFrame frame, JButton clear) 
+      {
+    frame.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            int frameWidth = frame.getWidth();
+            int frameHeight = frame.getHeight();
+            int buttonWidth = clear.getWidth();
+            int buttonHeight = clear.getHeight();
+            int x = (frameWidth - buttonWidth) / 2;
+            int y = (frameHeight - buttonHeight) / 2;
+            int xOffset = 0; // Specify the desired horizontal offset from the center
+            int yOffset = 10; // Specify the desired vertical offset from the center
+            clear.setBounds(x + xOffset+150, y + yOffset+20,150,30);
+            clear.setBorder(BorderFactory.createLineBorder(new Color(139, 0, 0), 2));
+        }
+    });
+}
+      //SIGNUP BUTTON Custom
+      public static void myCustom6(JFrame frame, JButton signUp) 
+      {
+    frame.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            int frameWidth = frame.getWidth();
+            int frameHeight = frame.getHeight();
+            int buttonWidth = signUp.getWidth();
+            int buttonHeight = signUp.getHeight();
+            int x = (frameWidth - buttonWidth) / 2;
+            int y = (frameHeight - buttonHeight) / 2;
+            int xOffset = 0; // Specify the desired horizontal offset from the center
+            int yOffset = 10; // Specify the desired vertical offset from the center
+            signUp.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0), 3));
+            signUp.setBounds(x + xOffset, y + yOffset-10,150,30);
+        }
+    });
+}
+      //RESET BUTTON custom
+       public static void myCustom7(JFrame frame, JButton reset)
+       {
+    frame.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            int frameWidth = frame.getWidth();
+            int frameHeight = frame.getHeight();
+            int buttonWidth = reset.getWidth();
+            int buttonHeight = reset.getHeight();
+            int x = (frameWidth - buttonWidth) / 2;
+            int y = (frameHeight - buttonHeight) / 2;
+            int xOffset = 0; // Specify the desired horizontal offset from the center
+            int yOffset = 10; // Specify the desired vertical offset from the center
+            reset.setBounds(x + xOffset, y + yOffset+50,150,30);
+            reset.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0), 3));
+        }
+    });
+}
+    
+    
+      
+      
+      //ADDED Constraints to add only numbers
+      public static void addNumericInputConstraint(JTextField textField)
+      {
+    ((PlainDocument) textField.getDocument()).setDocumentFilter(new DocumentFilter() {
+        @Override
+        public void insertString(FilterBypass fb, int offset, String text, AttributeSet attr) throws BadLocationException {
+            if (text.matches("\\d+")) {
+                super.insertString(fb, offset, text, attr);
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+            if (text.matches("\\d+")) {
+                super.replace(fb, offset, length, text, attrs);
+            }
+        }
+    });
+}
+      //GIVE MESSAGE TO TYPE ONLY NUMBER
+        public static void addNumericInputConstraint_message(JTextField textField) 
+        {
+    ((AbstractDocument) textField.getDocument()).setDocumentFilter(new DocumentFilter() {
+        @Override
+        public void insertString(FilterBypass fb, int offset, String text, AttributeSet attr) throws BadLocationException {
+            if (text.matches("\\d+")) {
+                super.insertString(fb, offset, text, attr);
+            } else {
+                showErrorMessage();
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+            if (text.matches("\\d+")) {
+                super.replace(fb, offset, length, text, attrs);
+            } else {
+                showErrorMessage();
+            }
+        }
+
+        private void showErrorMessage() {
+            JOptionPane.showMessageDialog(null, "Please type Numeric value");
+        }
+    });
+}
+        //MAKE NUMERS BOLD
+        public static void addNumericInputConstraint_BOLD(JTextField textField) 
+        {
+    Font originalFont = textField.getFont();
+    Font boldFont = new Font(originalFont.getFontName(), Font.BOLD, originalFont.getSize());
+
+    textField.getDocument().addDocumentListener(new DocumentListener() {
+        private void setBoldFont() {
+            SwingUtilities.invokeLater(() -> {
+                String text = textField.getText();
+                Font font = text.matches("\\d+") ? boldFont : originalFont;
+                textField.setFont(font);
+            });
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            setBoldFont();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            setBoldFont();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            // Not used for plain text documents
+        }
+    });
+}
+        //Make screen refresh/reset/clear
+        public static void addClearButtonFunctionality(JFrame frame, JButton clear) 
+        {
+    clear.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.dispose(); // Close the current frame
+            new Login(); // Create a new instance of the Login class to open a new frame
+        }
+    });
+}
+        //User can not minimize my screen less then 800 by 600
+       public static void enforceMinimumFrameSize(JFrame frame, int minWidth, int minHeight)
+       {
+    frame.setMinimumSize(new Dimension(minWidth, minHeight));
+}
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+
+
+      
+      
+     
+      
+      
+    // Custom JPanel class to draw the background image
+    static class ImagePanel extends JPanel 
+    {
+        private final Image backgroundImage;
+
+        public ImagePanel(Image image) {
+            backgroundImage = image;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()==login)
+        {
+            
+        }
+        else if(ae.getSource()==signUp)
+        {
+            
+        }
+        else if(ae.getSource()==clear)
+        {
+            DocumentFilter documentFilter = ((AbstractDocument) cardInput.getDocument()).getDocumentFilter();
+        ((AbstractDocument) cardInput.getDocument()).setDocumentFilter(null);
+        cardInput.setText("");
+        ((AbstractDocument) cardInput.getDocument()).setDocumentFilter(documentFilter);
+
+        documentFilter = ((AbstractDocument) pinInput.getDocument()).getDocumentFilter();
+        ((AbstractDocument) pinInput.getDocument()).setDocumentFilter(null);
+        pinInput.setText("");
+        cardInput.setText("");
+        ((AbstractDocument) pinInput.getDocument()).setDocumentFilter(documentFilter);
+        }
+            
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Login::new);
+    }
+}
---
+++ src/bank/management/system/SignUpOne.java
@@ -0,0 +1,315 @@
+package bank.management.system;
+
+
+import static bank.management.system.Login.enforceMinimumFrameSize;
+import static bank.management.system.Login.myCustom;
+import static bank.management.system.Login.myCustom2;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+import javax.swing.SwingUtilities;
+
+import com.toedter.calendar.JDateChooser;
+
+
+
+
+
+public class SignUpOne {
+    
+    JLabel formNumber,personalDetails,Name,FName,DOB,Gender,EAddress,MStatus,Address,City,State,Pin;
+    JTextField  EAddress_Input,Name_Input,FName_Input,Address_Input,CAddress_Input,State_Input,Pin_Input;
+    JDateChooser DOB_Chooser;
+    JRadioButton Male,Female,Married,UnMarried,Other;
+    ButtonGroup buttonGroup,M_status;
+    SignUpOne()
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        JFrame frame = new JFrame("SUPER ANY TIME MACINE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setContentPane(new Login.ImagePanel(new ImageIcon(getClass().getResource("/icons/meramann.png")).getImage()));
+        frame.setLayout(null);
+        frame.setSize(700, 850);
+       enforceMinimumFrameSize(frame,700,850);
+        frame.setLocationRelativeTo(null); // Center the frame
+        frame.setVisible(true);
+        
+        
+        //HEADING APPLICATION FORM
+        Random randomNumber=new Random();
+        long rand=Math.abs((randomNumber.nextLong()%9000L)+1000L);
+        formNumber=new JLabel("APPLICATION FORM : "+rand);
+        formNumber.setFont(new Font("Osward",Font.BOLD,40));
+        formNumber.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom(frame,formNumber,20);
+        frame.add(formNumber);
+        frame.setResizable(true);
+        
+        
+        //Personal details
+        personalDetails=new JLabel("PAGE 1: PERSONAL DETAILS");
+        personalDetails.setFont(new Font("Osward",Font.BOLD,30));
+        personalDetails.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,personalDetails,0,80);
+        frame.add(personalDetails);
+        
+        
+        //NAME
+        Name=new JLabel("NAME :");
+        Name.setFont(new Font("Osward",Font.BOLD,20));
+        Name.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,Name,180,150);
+        frame.add(Name);
+        //NAME INPUT
+        Name_Input=new JTextField();
+        myCustom2_Input(frame,Name_Input,145,250,30);
+        Name_Input.setBorder(BorderFactory.createLineBorder(Name_Input,3));
+        frame.add(Name_Input);
+       
+        
+
+
+        //FATHER NAME
+        FName=new JLabel("FATHER's NAME :");
+        FName.setFont(new Font("Osward",Font.BOLD,20));
+        FName.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,FName,180,210);
+         frame.add(FName);
+         //FATHER NAME
+         FName_Input=new JTextField();
+        myCustom2_Input(frame,FName_Input,205,250,30);
+        addBlackBorder(FName_Input,3);
+        frame.add(FName_Input);
+        
+        
+         //DOB 
+        DOB=new JLabel("DATE OF YEAR  :");
+        DOB.setFont(new Font("Osward",Font.BOLD,20));
+        DOB.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,DOB,180,270);
+        frame.add(DOB);
+        //DOB Chooser
+        DOB_Chooser=new JDateChooser();
+        myCustom3_DOB(frame,DOB_Chooser,265,250,30);
+        frame.setForeground(new Color(105,105,105));
+        DOB_Chooser.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        frame.add(DOB_Chooser);
+        
+        
+        
+    
+        
+         //Gender
+        Gender=new JLabel("GENDER :");
+        Gender.setFont(new Font("Osward",Font.BOLD,20));
+        Gender.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,Gender,180,330);
+        frame.add(Gender);
+        
+        
+        
+        //RADIO BUTTON
+        Male =new JRadioButton("MALE");
+        myCustom4_Radio(frame,Male,335,20,20,35);
+        frame.add(Male);
+        Female =new JRadioButton();
+        myCustom4_Radio(frame,Female,335,20,20,120);
+        frame.add(Female);
+        buttonGroup=new ButtonGroup();
+        buttonGroup.add(Male);
+        buttonGroup.add(Female);
+        
+
+    
+        
+
+
+
+
+        
+        
+        
+         //EMAIL ADDRESS
+        EAddress=new JLabel("EMAIL ADDRESS :");
+        EAddress.setFont(new Font("Osward",Font.BOLD,20));
+        EAddress.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,EAddress,180,390);
+        frame.add(EAddress);
+        //EMAIL INPUT
+        EAddress_Input=new JTextField();
+        myCustom2_Input(frame,EAddress_Input,385,250,30);
+        addBlackBorder(EAddress_Input,3);
+        frame.add(EAddress_Input);
+       
+        
+        
+         //MARITAL STATUS
+        MStatus=new JLabel("MARITAL STATUS :");
+        MStatus.setFont(new Font("Osward",Font.BOLD,20));
+        MStatus.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,MStatus,180,450);
+        frame.add(MStatus);
+        //RardioButton for M_status
+        Married =new JRadioButton();
+        myCustom4_Radio(frame,Married,455,20,20,30);
+        frame.add(Married);
+        UnMarried =new JRadioButton();
+        myCustom4_Radio(frame,UnMarried,455,20,20,120);
+        frame.add(UnMarried);
+        Other =new JRadioButton();
+        myCustom4_Radio(frame,Other,455,20,20,210);
+        frame.add(Other);
+        M_status=new ButtonGroup();
+        M_status.add(Married);
+        M_status.add(UnMarried);
+        M_status.add(Other);
+        
+        
+         //ADDRESS
+        Address=new JLabel("ADDRESS :");
+        Address.setFont(new Font("Osward",Font.BOLD,20));
+        Address.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,Address,180,510);
+        frame.add(Address);
+        //ADDRESS INPUT
+        Address_Input=new JTextField();
+        myCustom2_Input(frame,Address_Input,505,250,30);
+        addBlackBorder(Address_Input,3);
+        frame.add(Address_Input);
+        
+        
+        //CITY
+        City=new JLabel("CITY :");
+        City.setFont(new Font("Osward",Font.BOLD,20));
+        City.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,City,180,570);
+        frame.add(City);
+        //CITY ADDRESS
+        CAddress_Input=new JTextField();
+        myCustom2_Input(frame,CAddress_Input,565,250,30);
+        addBlackBorder(CAddress_Input,3);
+        frame.add(CAddress_Input);
+        
+        //STATE
+        State=new JLabel("STATE :");
+        State.setFont(new Font("Osward",Font.BOLD,20));
+        State.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,State,180,630);
+        frame.add(State);
+        //STATE INPUT
+        State_Input=new JTextField();
+        myCustom2_Input(frame,State_Input,625,250,30);
+        addBlackBorder(State_Input,3);
+        frame.add(State_Input);
+        
+        //PIN CODE
+        Pin=new JLabel("PIN :");
+        Pin.setFont(new Font("Osward",Font.BOLD,20));
+        Pin.setForeground(new Color(250, 249, 246));//24,30,33
+        myCustom2(frame,Pin,180,690);
+        frame.add(Pin);
+        //PinINput
+        Pin_Input=new JTextField();
+        myCustom2_Input(frame,Pin_Input,685,250,30);
+        addBlackBorder(Pin_Input,3);
+        frame.add(Pin_Input);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+     
+     
+     
+    
+
+
+     //CUSTOM INPUT
+      public static void  myCustom2_Input(JFrame frame,JTextField Field,int CustomX,int CustomY,int CustomZ)
+    {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=Field.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 Field.setBounds(labelX, CustomX, CustomY, CustomZ);   
+             }
+         });
+    }
+      //CUSTOM DOB
+    public static void  myCustom3_DOB(JFrame frame,JDateChooser Field,int CustomX,int CustomY,int CustomZ)
+    {
+        frame.addComponentListener(new ComponentAdapter(){
+             @Override
+             public void componentResized(ComponentEvent e)
+             {
+                 Dimension labelSize=Field.getPreferredSize();
+                 int labelX=(frame.getWidth()-labelSize.width)/2;
+                 Field.setBounds(labelX+35, CustomX, CustomY, CustomZ);   
+              
+             }
+         });
+    }
+  
+
+
+    //RADIO BUTTON
+public static void myCustom4_Radio(JFrame frame, JRadioButton radioButton, int CustomX, int CustomY, int CustomZ, int x) {
+    int radius_size=10;
+       radioButton.setOpaque(false); // Set the radio button background to transparent
+    radioButton.setBorderPainted(false); // Hide the border
+
+    frame.addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            int size = radius_size * 2;
+            radioButton.setBounds((frame.getWidth() - size) / 2 + x, CustomX, size, size);
+        }
+    });
+}
+
+
+
+
+
+
+//Custom Border for InputText
+
+
+ 
+
+
+
+     
+    
+    public static void main(String args[])
+    {
+        SwingUtilities.invokeLater(SignUpOne::new);
+    }
+}
